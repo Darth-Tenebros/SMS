4c4c8002bd17e5d2c33ec28091fb519d786af56c
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-08-30 10:29:21 +0200

    serve on port 3000

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const router = require('./routes/routes');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use('/', router);
 
