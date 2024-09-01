0bad21fba4c3f2c21c0b3dabb52a2f5dc0336191
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-01 23:18:16 +0200

    homeRoom repo

---
+++ backend/database/homeRoom.database.js
@@ -0,0 +1,24 @@
+
+const model = require('../models/sms.models');
+
+exports.getAllHomeRooms = () => {
+    return model.HomeRoomModel.find({});
+}
+
+exports.getHomeRoomById = (id) => {
+    return model.HomeRoomModel.find({_id: id});
+}
+
+exports.createHomeRoom = (homeRoom) => {
+    return model.HomeRoomModel({
+        name: homeRoom.name,
+        teacher: homeRoom.teacher
+    }).save();
+}
+
+exports.updateHomeRoom = (id, updatedData) => {
+    return model.HomeRoomModel.updateOne(
+        {_id: id},
+        {$set: updatedData}
+    );
+}
