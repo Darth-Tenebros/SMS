126052559b1a098deefedcbdd70f38c18d348ae8
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-02 15:18:06 +0200

    generate jwt tokens

--- backend/middleware/auth/auth.utils.js
+++ backend/middleware/auth/auth.utils.js
@@ -1,9 +1,28 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 
 const hashPassword = (password) => {
-    const salt = bcrypt.genSalt();
-    const hash = bcrypt.hash(password, salt);
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(password, salt);
     return hash;
 }
 
+
+const generateJWTToken = (user) => {
+    // TODO: store in .env
+    const SECRET_KEY = 'supersecuresecretkey';
+    return jwt.sign(
+        {
+            id: user._id,
+            role: user.role
+        },
+        SECRET_KEY,
+        {
+            expiresIn: '1h'
+        }
+    );
+}
+
+// console.log(generateJWTToken({_id: "1234", role:"student"}));
+exports.hashPassword = hashPassword;
+exports.generateJWTToken = generateJWTToken;
