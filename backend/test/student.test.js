d6829679a5b5ce6cf0dc951744001c337a3536a4
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-04 15:51:14 +0200

    more tests

--- backend/test/student.test.js
+++ backend/test/student.test.js
@@ -4,6 +4,21 @@ const expect = require('chai').expect;
 // let response;
 
 describe('test login', () => {
+    it('should return a 401 on /login/ with wrong creds', async () => {
+        const response = await supertest
+            .post('/login')
+            .send({
+                "email": "johnson@example.com",
+                "password": "password9"
+            })
+            .then(response => {
+                return response
+            })
+        expect(response.statusCode).to.be.equal(401);
+    });
+});
+
+describe('test login', () => {
     it('should return a 200 on /login/', async () => {
         const response = await supertest
             .post('/login')
@@ -17,6 +32,37 @@ describe('test login', () => {
         expect(response.statusCode).to.be.equal(200)
     });
 });
+
+describe('test get students', () => {
+    it('should return a 403 response on GET /students/ with no token provided', async () => {
+            
+        const response = await supertest
+            .get('/students/')
+            .then(response => {
+                return response;
+            });
+
+        expect(response.statusCode).to.be.equal(403);
+    });
+});
+
+
+describe('test get students', () => {
+    it('should return a 401 response on GET /students/ with invalid token', async () => {
+            
+        const response = await supertest
+            .get('/students/')
+            .set('Authorization', "Bearer "+"randombullshit")
+            .then(response => {
+                return response;
+            });
+
+        expect(response.statusCode).to.be.equal(401);
+    });
+
+
+});
+
 
 describe('test get students', () => {
     it('should return a 200 response on GET /students/', async () => {
